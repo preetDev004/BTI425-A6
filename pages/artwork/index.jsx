f143977944dfcb1b2f5c418d2337cdc076f52fca
preet patel <preetpatelofficial004@gmail.com>
2024-04-10 22:47:02 -0400

    fixing delpoyment error2

--- pages/artwork/index.jsx
+++ pages/artwork/index.jsx
@@ -1,4 +1,4 @@
-import validObjectIDList from '@/public/data/validObjectIDList.json'
+import validObjectIDList from '../public/data/validObjectIDList.json'
 import ArtworkCard from "@/components/ArtworkCard";
 import Error from "next/error";
 import { useRouter } from "next/router";
